bf914ce5524903c56a1635b5e5a0fbf4c4ec2a0b
Kunio Sawatani <devel@fathens.org>
2017-02-05 18:37:36 +0900

    カラム id は AutoInc を無条件で指定

--- database/codegen/CustomCodeGen.scala
+++ database/codegen/CustomCodeGen.scala
@@ -1,9 +1,10 @@
 import slick.driver.JdbcProfile
 import scala.concurrent.ExecutionContext.Implicits.global
-import slick.driver.MySQLDriver.api._
-import slick.driver.MySQLDriver
+import slick.driver.PostgresDriver.api._
+import slick.driver.PostgresDriver
 import slick.driver.JdbcProfile
 import slick.{model => m}
+import slick.ast.ColumnOption
 import slick.codegen.SourceCodeGenerator
 import slick.model.Model
 import scala.concurrent.duration.Duration
@@ -11,13 +12,13 @@ import scala.concurrent.{Await, ExecutionContext}
 
 class CustomGenerator(model: m.Model) extends SourceCodeGenerator(model) {
   // add some custom imports
-  override def code = "import com.github.tototoshi.slick.PostgresJodaSupport._\n" + "import org.joda.time.DateTime\n" + super.code
+  override def code = "import com.github.tototoshi.slick.PostgresJodaSupport._\n" + super.code
   override def Table = new Table(_) {
     override def autoIncLastAsOption = true
     override def Column = new Column(_) {
       override def rawType = model.tpe match {
-        case "java.sql.Timestamp" => "DateTime"
-        case "java.sql.Date" => "DateTime"
+        case "java.sql.Timestamp" => "org.joda.time.DateTime"
+        case "java.sql.Date" => "org.joda.time.DateTime"
         case _ => {
           super.rawType
         }
@@ -43,11 +44,18 @@ object CustomCodeGen {
     val model = Await.result(db.run(driver.createModel(None, false)(ExecutionContext.global).withPinnedSession), Duration.Inf)
 
     // 登録日(create_at)と更新日(update_at)は Postgres 側の設定で対応するので Tables からは除外する
-    val ts = (for {
+    val ts = for {
       t <- model.tables
       if (t.name.table != "schema_version")
-      c = t.columns.filter(_.name != "create_at").filter(_.name != "update_at")
-    } yield(slick.model.Table(t.name, c, t.primaryKey, t.foreignKeys, t.indices, t.options)))
+      cs = for {
+        c <- t.columns
+      } yield {
+        c.name match {
+          case "id" => c.copy(options = c.options + ColumnOption.AutoInc)
+          case _ => c
+        }
+      }
+    } yield slick.model.Table(t.name, cs, t.primaryKey, t.foreignKeys, t.indices, t.options)
     val fModel = Model(tables = ts)
 
     val codeGenFuture = new CustomGenerator(fModel).writeToFile(slickDriver, outputFolder , pkg, "Tables", "Tables.scala")
